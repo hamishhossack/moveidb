eaec319fe1e9fdefa4d03b49b7ba3dd15c9d764e
Hamish Hossack <hamish.hossack@essensys.tech>
2017-05-22 20:15:19 +0100

    fix(app): Include the app initialise into the index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import App from './app';
+/**
+ * Import the app styles for webpack
+ */
+import './scss/main.scss';
 
+const AppComponent = new App();
+
+window.onload = () => {
+  console.debug(`Loading App`);
+  AppComponent.init();
+};
