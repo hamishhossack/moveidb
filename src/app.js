7728ba12c2f09a21cc3d17f9f124289feab9141e
Hamish Hossack <hamish.hossack@essensys.tech>
2017-05-22 20:18:14 +0100

    feat(moviedb): MovieDbService with authentication

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
-export default class App {
+import Component from './components/component';
+import theMovieDb from './services/moviedb';
+
+import appTpl from './app.hbs';
+
+export default class App extends Component {
   constructor() {
+    super();
+    this.name = 'main';
+    this.tpl = appTpl;
+
+    theMovieDb.authentication.generateToken(
+      (token) => sessionStorage.setItem('token', token),
+      console.error
+    );
   }
 }
---
+++ src/services/moviedb.js
@@ -0,0 +1,8 @@
+import theMovieDb from 'themoviedb-javascript-library';
+
+theMovieDb.common.api_key = 'b0ac7e50a0e861bd5f8456035283d649';
+theMovieDb.common.base_uri = 'https://api.themoviedb.org/3/';
+theMovieDb.common.images_uri = 'https://image.tmdb.org/t/p/';
+theMovieDb.common.timeout = 2000;
+
+export default theMovieDb;
