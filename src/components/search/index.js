ebb04e3282da1eed25b1bf75efe558991c0ea3c4
Hamish Hossack <hamish.hossack@essensys.tech>
2017-05-23 11:56:47 +0100

    fix(search): Search observable not registered

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -19,13 +19,12 @@ export default class SearchComponent extends Component {
   bindEvents() {
     // Watch the input and apply execute the latest search (wait time for human typing)
     const $input = this.el.querySelector('input');
-    const search = Observable.fromEvent($input, 'keyup')
+    Observable.fromEvent($input, 'keyup')
       .pluck('target', 'value')
       .filter(text => text.length > 2)
       .distinctUntilChanged()
-      .debounceTime(300);
-
-    // Add our new query to the search service
-    search.do(query => this.currentMoviesService.searchQuery = query);
+      .debounceTime(300)
+      .do(query => this.currentMoviesService.searchQuery = query)
+      .subscribe();
   }
 }
