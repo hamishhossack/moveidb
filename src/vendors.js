55b142f00fa24f8a29cb50ed46ffb7d11dd7ab7f
Hamish Hossack <hamish.hossack@essensys.tech>
2017-05-25 08:39:11 +0100

    refactor(app): install font-awesome,animate.css, cleanse app styles, hbs helpers

--- src/vendors.js
+++ src/vendors.js
@@ -3,10 +3,4 @@
  */
 
 import 'rxjs/Rx';
-
-import Handlebars from 'handlebars';
-
-Handlebars.registerHelper(
-  'truncate',
-  (str, length = 150) => str.length > length ? `${str.substring(0, length)}...` : str
-);
+import 'handlebars';
