05891d67f955fa737d87d53893a2e717c69e9f6c
Hamish Hossack <hamish.hossack@essensys.tech>
2017-06-01 11:28:22 +0100

    refactor(visual): Selected movie styles

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './scss/main.scss';
 
 const AppComponent = new App();
 
-window.onload = () => {
-  console.debug(`Loading App`);
+document.addEventListener('DOMContentLoaded', () => {
+  console.debug('Loading App');
   AppComponent.init();
-};
+});
